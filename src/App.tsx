b1d826ea7f4817f5a007dc6753d4bf5aefbe341a
noodles-1 <adrianeroa081202@gmail.com>
2024-12-19 08:43:15 +0800

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import './App.css'
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+import Button from 'react-bootstrap/Button';
 
 function App() {
+    const [counter, setCounter] = useState<number>(0);
+
     return (
         <>
-            <h1> hello world </h1>
+            <div className='bg-white shadow border p-3 rounded-2 text-start' style={{ width: "200px "}}>
+                <h5> Counter: {counter} </h5>
+                <div className='d-flex justify-content-between w-50'>
+                    <Button variant='secondary' className='bg-danger px-3' onClick={() => setCounter(counter - 1)}> - </Button>
+                    <Button variant='secondary' className='bg-primary' onClick={() => setCounter(counter + 1)}> + </Button>
+                </div>
+            </div>
         </>
     )
 }
